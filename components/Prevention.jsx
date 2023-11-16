3d234f4c9d0197e163a39bba3d1742e9fac56e4d
Luis Landaeta <luis.landaeta24@gmail.com>
2023-11-16 15:10:11 -0400

    .

--- components/Prevention.jsx
+++ components/Prevention.jsx
@@ -10,7 +10,7 @@ const Prevention = () => {
         alt="Kids smiling"
         className="h-auto w-full lg:hidden 2xl:block"
       />
-      <section className="bg-prevention hidden 2xl:block">
+      <section className="bg-prevention hidden md:block">
         <div className="paragraphs-display h-full place-content-center">
           <h2 className="subheadline">Prevention Over Treatment</h2>
           <p>
@@ -22,13 +22,6 @@ const Prevention = () => {
             invasive and time-efficient alternative.
           </p>
           <p>
-            Furthermore, prevention can also decelerate the progression of
-            cavities, reducing the necessity for complex treatments and
-            mitigating the stress of dental appointments. In addition, it proves
-            to be cost-effective in comparison to expensive treatments that may
-            not be fully covered by insurance.
-          </p>
-          <p>
             Moreover, prevention preserves a child&apos;s natural teeth and
             upholds their alignment and bite integrity. By instilling preventive
             practices at home, children acquire valuable oral hygiene and
@@ -38,27 +31,20 @@ const Prevention = () => {
           </p>
         </div>
       </section>
-      <section className="bg-prevention 2xl:hidden">
+      <section className="bg-prevention lg:hidden">
         <Image
           src={kids}
           alt="Kids smiling"
-          className="float-left m-6 hidden w-[50%] lg:inline 2xl:hidden"
+          className="float-left m-6 hidden w-[50%] md:inline"
         />
-        <h2 className="subheadline mb-6">Prevention Over Treatment</h2>
-        <p>
+        <h2 className="subheadline">Prevention Over Treatment</h2>
+        <p className="my-4">
           Preventing tooth decay in children yields numerous advantages,
           including the avoidance of discomfort, stress, and expenses, alongside
           the promotion of good oral habits and overall well-being. Preventative
           measures spare children from the pain and anxiety associated with
           dental procedures such as fillings, offering a less invasive and
           time-efficient alternative.
-        </p>
-        <p className="my-4">
-          Furthermore, prevention can also decelerate the progression of
-          cavities, reducing the necessity for complex treatments and mitigating
-          the stress of dental appointments. In addition, it proves to be
-          cost-effective in comparison to expensive treatments that may not be
-          fully covered by insurance.
         </p>
         <p>
           Moreover, prevention preserves a child&apos;s natural teeth and
