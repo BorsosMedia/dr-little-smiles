0c639bcdc90223de253fcc2e932df8db36cb62d7
Luis Landaeta <luis.landaeta24@gmail.com>
2023-11-20 10:25:54 -0400

    Last changes requested by client

--- components/Reviews.jsx
+++ components/Reviews.jsx
@@ -3,7 +3,7 @@ import ReviewCard from "./ReviewCard";
 const Reviews = () => {
   return (
     <article className="factors-display bg-white">
-      <h2 className="subheadline blue">See What Our Patients Are Saying</h2>
+      <h2 className="subheadline blue">See What Others Are Saying About Us</h2>
       <section className="flex-center flex-wrap gap-32">
         <ReviewCard
           name="John Khoushoo"
@@ -16,8 +16,8 @@ const Reviews = () => {
           review="“Dr.Nik has been a great help to me from day one as he made me aware of when I need to start looking after my son's teeth. He has continued to provide lots of helpful tips and suggestions through his social media platforms. He is also always readily available to respond to any queries I've had and is very knowledgeable. He clearly knows and understands children which is what makes his advice so practical and helpful. Thank you for your help Dr. Nik!”"
         />
         <ReviewCard
-          name="Gab Lemay"
-          location="Quebec, Canada"
+          name="Gabrielle Lemay"
+          location="Trois-Rivières, Quebec, Canada"
           review="“Dr Sharma has been a gem for me. As a mother of a toddler and a dentist, he has been my go to resource when I need information about prevention, good dental hygiene, diagnosis and even resources for treatments. He is quick to provide evidence based ressources and information that are available and easy to understand and apply. He provides interesting information about dental products and how to use them, for dentists and parents. And always with a smile!”"
         />
       </section>
